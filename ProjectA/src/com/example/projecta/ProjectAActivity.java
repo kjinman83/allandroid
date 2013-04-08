f27e116cb8a88fc23e1d8a85409e481fccf29a51
kjinman83 <kjinman83@naver.com>
2013-04-08 18:17:16 +0900

    ProjectA

---
+++ ProjectA/src/com/example/projecta/ProjectAActivity.java
@@ -0,0 +1,51 @@
+package com.example.projecta;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class ProjectAActivity extends Activity implements OnClickListener{
+	static final int[] BUTTONS = 
+	{
+		R.id.button1,
+		R.id.button2,
+		R.id.button3
+	};
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		for(int btnId:BUTTONS){
+			Button tmpButton = (Button) findViewById(btnId);
+			tmpButton.setOnClickListener(this);
+		}
+		
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch(v.getId()){
+			case R.id.button1:
+				Toast.makeText(ProjectAActivity.this, "가위", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.button2:
+				Toast.makeText(ProjectAActivity.this, "바위", Toast.LENGTH_SHORT).show();
+				break;
+			case R.id.button3:
+				Toast.makeText(ProjectAActivity.this, "보", Toast.LENGTH_SHORT).show();
+				break;
+		}
+	}
+	
+
+}
