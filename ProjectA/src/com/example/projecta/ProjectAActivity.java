7191b69d58222bd40bf0fb7a4f8aaf0e77e7c528
kjinman83 <kjinman83@naver.com>
2013-04-09 09:32:22 +0900

    day0409

--- ProjectA/src/com/example/projecta/ProjectAActivity.java
+++ ProjectA/src/com/example/projecta/ProjectAActivity.java
@@ -1,13 +1,14 @@
 package com.example.projecta;
 
 import java.util.Random;
-
+import com.example.projecta.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ProjectAActivity extends Activity implements OnClickListener, OnLongClickListener{
@@ -17,41 +18,44 @@ public class ProjectAActivity extends Activity implements OnClickListener, OnLon
 		R.id.button2,
 		R.id.button3
 	};
+	
 	static final String gawi[] = {"가위", "바위", "보"};
+	TextView textview1;
+	TextView textview2;
+	int win = 0;
+	int lose = 0;
+	int same = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
+		textview1 = (TextView) findViewById(R.id.text1);
+		textview2 = (TextView) findViewById(R.id.text2);
 		for(int btnId:BUTTONS){
 			Button tmpButton = (Button) findViewById(btnId);
 			tmpButton.setOnClickListener(this);
 			
 			tmpButton.setOnLongClickListener(this);
 		}
-		
 	}
 
 	@Override
 	public void onClick(View v) {
 		Random r = new Random();
-		int computer = r.nextInt(3);
-		
+		int computer = r.nextInt(3);		
 		
 		// TODO Auto-generated method stub
 		switch(v.getId()){
 			case R.id.button1:
 				ResultGame(0, computer);
-				//Toast.makeText(ProjectAActivity.this, "가위", Toast.LENGTH_SHORT).show();
 				break;
 			case R.id.button2:
 				ResultGame(1, computer);
-				//Toast.makeText(ProjectAActivity.this, "바위", Toast.LENGTH_SHORT).show();
 				break;
 			case R.id.button3:
 				ResultGame(2, computer);
-				//Toast.makeText(ProjectAActivity.this, "보", Toast.LENGTH_SHORT).show();
 				break;
 		}
 	}
@@ -75,42 +79,60 @@ public class ProjectAActivity extends Activity implements OnClickListener, OnLon
 	}
 	
 	
-	public void ResultGame(int i, int j){
-		//i= 내꺼, j= 컴퓨터꺼
-		//가위=0, 바위=1, 보=2		
+	public void ResultGame(int me, int com){
+		//me= 내꺼, com= 컴퓨터꺼
+		//가위=0, 바위=1, 보=2
 		
-		switch(i){
+		String result = "";
+		String game = "";
+		
+		switch(me){
 		case 0:
-			if(j == 0){
-				Toast.makeText(ProjectAActivity.this, "비겼습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
-			}else if(j == 1){
-				Toast.makeText(ProjectAActivity.this, "졌습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
-			}else if(j == 2){
-				Toast.makeText(ProjectAActivity.this, "이겼습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
+			if(com == 0){
+				result = "비겼습니다.";
+				same++;
+			}else if(com == 1){
+				result = "졌습니다.";				
+				lose++;
+			}else if(com == 2){				
+				result = "이겼습니다.";
+				win++;
 			}			
 			
 			break;
 		case 1:
-			if(j == 0){
-				Toast.makeText(ProjectAActivity.this, "이겼습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
-			}else if(j == 1){
-				Toast.makeText(ProjectAActivity.this, "비겼습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
-			}else if(j == 2){
-				Toast.makeText(ProjectAActivity.this, "졌습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
+			if(com == 0){
+				result = "이겼습니다.";
+				win++;
+			}else if(com == 1){
+				result = "비겼습니다.";
+				same++;
+			}else if(com == 2){
+				result = "졌습니다.";
+				lose++;
 			}	
 			break;
 		case 2:
-			if(j == 0){
-				Toast.makeText(ProjectAActivity.this, "졌습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
-			}else if(j == 1){
-				Toast.makeText(ProjectAActivity.this, "이겼습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
-			}else if(j == 2){
-				Toast.makeText(ProjectAActivity.this, "비겼습니다." + " 나:" + gawi[i] + " 컴퓨터:" + gawi[j], Toast.LENGTH_SHORT).show();
+			if(com == 0){
+				result = "졌습니다.";
+				lose++;
+			}else if(com == 1){
+				result = "이겼습니다.";
+				win++;
+			}else if(com == 2){
+				result = "비겼습니다.";
+				same++;
 			}	
 			break;
 		}
 		
+		result += "ME>" + gawi[me] + " COMPUTER>" + gawi[com];
+		game = win+"승"+same+"무"+lose+"패";
+				
+		textview1.setText(result);
+		textview2.setText(game);
+		
+		
 	}
-	
 
 }
